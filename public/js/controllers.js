c9aed5f1da394e24a45d46eeee213ce48efa183e
Gordon Murray Dent <gordonmdent@gmail.com>
2013-09-29 01:04:09 +0000

    add directive for swiper

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -7,33 +7,8 @@ angular.module('myApp.controllers', []).
     //parent controller
   }).
   controller('IndexController', function ($scope, $http) {
-    $http.get('/api/0/messages').
-    success(function (data) {
-      $scope.messages = data;
-      console.log(data);
-    });
-  }).
-  controller('ViewController', function ($scope, $http, $routeParams) {
-    $http.get('/api/0/messages/'+$routeParams.id).
-    success(function (data) {
-      $scope.message = data;
-    });
-
+    console.log('Choose');
   }).
   controller('IndexController2', function ($scope, $http) {
-    var elem = document.getElementById('mySwipe');
-    window.mySwipe = Swipe(elem, {
-      startSlide: 4,
-      //auto: 3000,
-      continuous: true,
-      disableScroll: true,
-      stopPropagation: true,
-      callback: function(index, element) {},
-      transitionEnd: function(index, element) {}
-    });
-    $http.get('/api/0/messages').
-    success(function (data) {
-      $scope.messages = data;
-      console.log(data);
-    });
+    console.log('Swipe like mad');
   });
--- public/js/directives.js
+++ public/js/directives.js
@@ -2,12 +2,28 @@
 
 /* Directives */
 
-angular.module('myApp.directives', []).
-  directive('appVersion', function (version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }).
-  directive('leaderboard', function() {
+angular.module('myApp.directives', [])
+	.directive('appVersion', function (version) {
+	    return function(scope, elm, attrs) {
+	      elm.text(version);
+	    };
+	  })
+  .directive('swiper', function() {
+	  	return function(scope, elem) {
+	  		console.log('adding swipe');
+	  		console.log(elem);
+	  		console.log(elem[0]);
+		    window.mySwipe = Swipe(elem[0], {
+		      startSlide: 4,
+		      //auto: 3000,
+		      continuous: true,
+		      disableScroll: true,
+		      stopPropagation: true,
+		      callback: function(index, element) {},
+		      transitionEnd: function(index, element) {}
+		    });
+	  	};
+  })
+  .directive('leaderboard', function() {
 
   });
