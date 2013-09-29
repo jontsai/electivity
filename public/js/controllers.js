d59d3b7238fdc99691f43404afa7bfc6b9be6b99
saratheneale <test@no.com>
2013-09-28 18:21:07 -0700

    playing around with directives

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -11,4 +11,16 @@ angular.module('myApp.controllers', []).
   }).
   controller('IndexController2', function ($scope, $http) {
     console.log('Swipe like mad');
+
+    // var elem = document.getElementById('mySwipe');
+    // window.mySwipe = Swipe(elem, {
+    //   startSlide: 4,
+    //   //auto: 3000,
+    //   continuous: true,
+    //   disableScroll: true,
+    //   stopPropagation: true,
+    //   callback: function(index, element) {},
+    //   transitionEnd: function(index, element) {}
+    // });
+    // $scope.msg="Yahoo Hackinz"
   });
